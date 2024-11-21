905678a70b3dd82f1aa61b4037a039e46f6725db
Leo Git <leodomsolar@gmail.com>
2024-11-21 11:07:31 +0700

    Commit

---
+++ database/migrations/2024_11_21_031452_create_assistants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssistantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assistants', function (Blueprint $table) {
+            $table->id();
+            $table->string('assistant_id',255)->nullable();
+            $table->string('name',255);
+            $table->longText('instructions');
+            $table->text('tools');
+            $table->string('model',255)->default('gpt-4o-mini');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assistants');
+    }
+}
---
+++ database/migrations/2024_11_21_033312_create_rooms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('assistant_id');
+            $table->string('thread_id')->nullable();
+            $table->string('run_id')->nullable();
+            $table->timestamp('last_message_at')->nullable();
+            $table->text('last_message')->nullable();
+            $table->boolean('sort')->default(0);
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
